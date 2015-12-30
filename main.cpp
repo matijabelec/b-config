92697ff67249c039230169774245cdc1fe0d9639
matijabelec <mbelec@foi.hr>
2015-12-30 19:53:45 +0100

    initial project created

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: Matija Belec <matijabelec1@gmail.hr>
+ * 
+ * Created on December 30, 2015, 07:45 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    //TODO: implement
+    
+    return 0;
+}
+
